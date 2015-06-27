1b9858f53ed72dc8a57572687593dc77ef02d615
bryanapellanes <bryan.apellanes@gmail.com>
2015-06-27 11:46:59 -0700

    BamContent/Common/js is now bamvvm

---
+++ data/database.db.js
@@ -0,0 +1,26 @@
+﻿var database = {
+    nameSpace: "Bryan.Common.Data",
+    schemaName: "BryanCommon",
+	xrefs:[
+		// [ "LeftTableName": "RightTableName ] // array of arrays that have two values; defines many to many 
+	],
+    tables: [
+        {
+            name: "CommonItem",
+            fks: [],
+            cols: [
+                { Name: "String", Null: false }
+            ]            
+        },
+        {
+            name: "CommonSubItem",
+            fks: [
+                { CommonItemId: "CommonItem" }
+            ],
+            cols:[
+                { Name: "String", Null: false },
+                { Created: "DateTime" }
+            ]
+        }
+    ]
+}
---
+++ user.js
@@ -0,0 +1,9 @@
+﻿var user = (function(){
+    function ctor(name, isAuth){
+        this.name = name;
+
+        this.isAuthenticated = isAuth || false;
+    }
+
+    return ctor;
+})(jQuery, _);
